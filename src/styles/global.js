41e798338b05a904e27d22b1f6fecf0bd6316971
SantosMaycon <mayconalves.santos14@gmail.com>
2021-09-13 17:00:42 -0300

    config global styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -10,6 +10,7 @@ const GlobalStyles = createGlobalStyle`
     font-size: 62.5%;
   }
   body {
+    background-color: #f6f6f6;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 `
